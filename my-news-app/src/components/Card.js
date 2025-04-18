c8513a5791c6f239e0826c3bbbb3f87d1cc5d1a2
somyagupta05 <somyagupta052003@gmail.com>
2025-04-18 18:37:47 +0530

    working on ui

--- my-news-app/src/components/Card.js
+++ my-news-app/src/components/Card.js
@@ -1,23 +1,22 @@
-// import React from 'react'
 
-const Card = ({data=[]}) => {
-  console.log(data)
+const Card = ({ data }) => {
+  const safeData = Array.isArray(data) ? data : [];
+  console.log('Data:', safeData);
+
   return (
-    <div className='cardConatiner'>
-      {data.map((currItem,index)=>{
-        return(
-          <div className='card' key={index}>
-            <img src={currItem.urlToImage} alt="Card Image"/>
-            <div className="cardcontent">
-              <a>{currItem.title}</a>
-              <p>{currItem.description}</p>
-              <button>Read More</button>
-            </div>
+    <div className='cardContainer'>
+      {safeData.map((currItem, index) => (
+        <div className='card' key={index}>
+          <img src={currItem.urlToImage} alt="Card Image" />
+          <div className="cardContent">
+            <a className='title'>{currItem.title}</a>
+            <p>{currItem.description}</p>
+            <button>Read More</button>
           </div>
-        )
-      })}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
